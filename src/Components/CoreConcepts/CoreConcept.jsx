58dfac0c0f34a66cb9fd655ae30180758d57c343
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-02 18:44:43 +0100

    modified

--- src/Components/CoreConcepts/CoreConcept.jsx
+++ src/Components/CoreConcepts/CoreConcept.jsx
@@ -1,10 +1,18 @@
 import "./CoreConcept.css";
-export default function CoreConcept({ image, title, description }) {
+import { data } from "../../data";
+export default function CoreConcept() {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {data.map(({ image, title, description }) => (
+          <li key={title}>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
